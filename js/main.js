10af466c37bd22048ab984f6a3c6c25f3ce5214d
Olivier Daire <daire.olivier@gmail.com>
2015-01-11 16:43:15 +0100

    Dynamic conf Dropbox

--- js/main.js
+++ js/main.js
@@ -49,7 +49,7 @@ noteManager.prototype = {
             date = today[0],
             dropboxOptions = {
               files: [
-                  {'url': config.website_url+'WebNotes-'+id+'.json', 'filename': 'WebNotes_'+date+'.json'},
+                  {'url': document.url+'WebNotes-'+id+'.json', 'filename': 'WebNotes_'+date+'.json'},
               ],
               success: function () {
                 console.log('success');
