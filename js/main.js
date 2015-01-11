a87943322bf762c5666ca2f34e42a330191058ef
Olivier Daire <daire.olivier@gmail.com>
2015-01-11 16:38:44 +0100

    Update Dropbox conf

--- js/main.js
+++ js/main.js
@@ -49,7 +49,7 @@ noteManager.prototype = {
             date = today[0],
             dropboxOptions = {
               files: [
-                  {'url': 'http://localhost/Web-Notes/temp/WebNotes-'+id+'.json', 'filename': 'WebNotes_'+date+'.json'},
+                  {'url': config.website_url+'WebNotes-'+id+'.json', 'filename': 'WebNotes_'+date+'.json'},
               ],
               success: function () {
                 console.log('success');
